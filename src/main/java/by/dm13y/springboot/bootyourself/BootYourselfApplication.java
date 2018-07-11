a5f0c2f93880623e0ee2c2cee95431946ed017b8
dm13y <5132095@tut.by>
2018-07-11 23:39:49 +0300

    init project

---
+++ src/main/java/by/dm13y/springboot/bootyourself/BootYourselfApplication.java
@@ -0,0 +1,12 @@
+package by.dm13y.springboot.bootyourself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootYourselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootYourselfApplication.class, args);
+    }
+}
---
+++ src/test/java/by/dm13y/springboot/bootyourself/BootYourselfApplicationTests.java
@@ -0,0 +1,16 @@
+package by.dm13y.springboot.bootyourself;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootYourselfApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
